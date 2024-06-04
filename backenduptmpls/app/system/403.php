8714213816f6b2d17ba596d07c9cdabe3a506480
Dmitry Kiselev <dmitry@Dmitrys-MacBook-Air-v7.local>
2024-06-04 19:18:34 +0300

    work in progress

--- backenduptmpls/app/system/403.php
+++ backenduptmpls/app/system/403.php
@@ -16,7 +16,6 @@
 		<link rel="canonical" href="http://preview.keenthemes.comauthentication/layouts/corporate/sign-in.html" />
 		<link rel="shortcut icon" href="/assets/media/logos/favicon.ico" />
 		<!--begin::Fonts(mandatory for all pages)-->
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
 		<!--end::Fonts-->
 		<!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
 		<link href="/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
