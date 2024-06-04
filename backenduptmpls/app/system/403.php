ee272fb67a58c6b41de7107d9b2774fe0ef81c55
Dmitry Kiselev <dmitry@Dmitrys-MacBook-Air-v7.local>
2024-06-04 19:43:13 +0300

    work in progress

--- backenduptmpls/app/system/403.php
+++ backenduptmpls/app/system/403.php
@@ -1,142 +1,82 @@
 <!DOCTYPE html>
 <html lang="ru">
-	<!--begin::Head-->
 	<head>
 
-		<title>Keen - Multi-demo Bootstrap 5 HTML Admin Dashboard Template by KeenThemes</title>
+		<title>Вход в свой профиль – uptime+</title>
 		<meta charset="utf-8" />
-		<meta name="description" content="The most advanced Bootstrap Admin Theme on Bootstrap Market trusted by over 4,000 beginners and professionals. Multi-demo, Dark Mode, RTL support. Grab your copy now and get life-time updates for free." />
-		<meta name="keywords" content="keen, bootstrap, bootstrap 5, bootstrap 4, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+	
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta property="og:locale" content="en_US" />
-		<meta property="og:type" content="article" />
-		<meta property="og:title" content="Keen - Multi-demo Bootstrap 5 HTML Admin Dashboard Template by KeenThemes" />
-		<meta property="og:url" content="https://keenthemes.com/keen" />
-		<meta property="og:site_name" content="Keen by Keenthemes" />
-		<link rel="canonical" href="http://preview.keenthemes.comauthentication/layouts/corporate/sign-in.html" />
+	
 		<link rel="shortcut icon" href="/assets/media/logos/favicon.ico" />
-		<!--begin::Fonts(mandatory for all pages)-->
-		<!--end::Fonts-->
-		<!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
+		
 		<link href="/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
 		<link href="/assets/css/style.bundle.css?v=<?php echo time();?>" rel="stylesheet" type="text/css" />
-		<!--end::Global Stylesheets Bundle-->
-		<script>// Frame-busting to prevent site from being loaded within a frame without permission (click-jacking) if (window.top != window.self) { window.top.location.replace(window.self.location.href); }</script>
+		
 	</head>
-	<!--end::Head-->
-	<!--begin::Body-->
 	<body id="kt_body" class="app-blank app-blank">
-		<!--begin::Theme mode setup on page load-->
 		<script>var defaultThemeMode = "light"; var themeMode; if ( document.documentElement ) { if ( document.documentElement.hasAttribute("data-bs-theme-mode")) { themeMode = document.documentElement.getAttribute("data-bs-theme-mode"); } else { if ( localStorage.getItem("data-bs-theme") !== null ) { themeMode = localStorage.getItem("data-bs-theme"); } else { themeMode = defaultThemeMode; } } if (themeMode === "system") { themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"; } document.documentElement.setAttribute("data-bs-theme", themeMode); }</script>
-		<!--end::Theme mode setup on page load-->
-		<!--begin::Root-->
 		<div class="d-flex flex-column flex-root" id="kt_app_root">
-			<!--begin::Authentication - Sign-in -->
 			<div class="d-flex flex-column flex-lg-row flex-column-fluid">
-				<!--begin::Aside-->
 				<div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center" style="background-image: url(/assets/media/misc/auth-bg.png)">
-					<!--begin::Content-->
 					<div class="d-flex flex-column flex-center p-6 p-lg-10 w-100">
-						<!--begin::Logo-->
-						<a href="index.html" class="mb-0 mb-lg-20">
+						<a href="https://uptime.plus/" class="mb-0 mb-lg-20">
 							<img alt="Logo" src="/assets/media/logos/default-white.svg" class="h-40px h-lg-50px" />
 						</a>
 						<img class="d-none d-lg-block mx-auto w-300px w-lg-75 w-xl-300px mb-10 mb-lg-20" src="/assets/media/misc/auth-screens.png" alt="" />
 						<h1 class="d-none d-lg-block text-white fs-2qx fw-bold text-center mb-7">Мониторинг сайта и API</h1>
 						
-						<div class="d-none d-lg-block text-white fs-lg text-center">Сервис  <span class="opacity-75-hover text-warning fw-semibold me-1">uptime+</span>позволяет в режиме реального времени следить за своим сайтом, сервисом или API. <br />
+						<div class="d-none d-lg-block text-white fs-lg text-center">Сервис  <span class="text-warning fw-semibold me-1">uptime+</span>позволяет в режиме реального времени следить за своим сайтом, сервисом или API. <br />
 							Бывают разные ситуации, а вдруг сервер упал или деплой неудачный? <br /><br />Мы увидим и оповестим вас об этом в тот же час 😉</div>
 					</div>
 				</div>
 				<div class="d-flex flex-column flex-lg-row-fluid w-lg-50 p-10">
-					<!--begin::Form-->
 					<div class="d-flex flex-center flex-column flex-lg-row-fluid">
-						<!--begin::Wrapper-->
 						<div class="w-lg-500px p-10">
-							<!--begin::Form-->
 							<form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" data-kt-redirect-url="index.html" action="#">
-								<!--begin::Heading-->
 								<div class="text-center mb-11">
-									<!--begin::Title-->
 									<h1 class="text-gray-900 fw-bolder mb-3">Вход</h1>
-									<!--end::Title-->
-									<!--begin::Subtitle-->
 									<div class="text-gray-500 fw-semibold fs-6">в свой профиль</div>
-									<!--end::Subtitle=-->
 								</div>
 								
-								
-								<!--begin::Input group=-->
 								<div class="fv-row mb-8">
-									<!--begin::Email-->
 									<input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent" />
-									<!--end::Email-->
 								</div>
-								<!--end::Input group=-->
 								<div class="fv-row mb-3">
-									<!--begin::Password-->
 									<input type="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent" />
-									<!--end::Password-->
 								</div>
-								<!--end::Input group=-->
-								<!--begin::Wrapper-->
 								<div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
 									<div></div>
-									<!--begin::Link-->
-									<a href="authentication/layouts/corporate/reset-password.html" class="link-primary">Forgot Password ?</a>
-									<!--end::Link-->
+									<a href="/forgot" class="link-primary">Забыли пароль?</a>
 								</div>
-								<!--end::Wrapper-->
-								<!--begin::Submit button-->
 								<div class="d-grid mb-10">
 									<button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
-										<!--begin::Indicator label-->
 										<span class="indicator-label">Войти</span>
-										<!--end::Indicator label-->
-										<!--begin::Indicator progress-->
 										<span class="indicator-progress">Please wait... 
 										<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-										<!--end::Indicator progress-->
 									</button>
 								</div>
-								<!--end::Submit button-->
-								<!--begin::Sign up-->
 								<div class="text-gray-500 text-center fw-semibold fs-6">Еще не с нами? 
-								<a href="/sugnup" class="link-primary">Хочу зарегистрироваться</a></div>
-								<!--end::Sign up-->
+								<a href="/signup" class="link-primary">Хочу зарегистрироваться</a></div>
 							</form>
-							<!--end::Form-->
 						</div>
-						<!--end::Wrapper-->
 					</div>
-					<!--end::Form-->
-					<!--begin::Footer-->
+
 					<div class="d-flex flex-center flex-wrap px-5">
-						<!--begin::Links-->
 						<div class="d-flex fw-semibold text-primary fs-base">
-							<a href="https://keenthemes.com" class="px-5" target="_blank">Terms</a>
-							<a href="https://devs.keenthemes.com" class="px-5" target="_blank">Plans</a>
-							<a href="https://themes.getbootstrap.com/product/keen-the-ultimate-bootstrap-admin-theme/" class="px-5" target="_blank">Contact Us</a>
+							<a href="https://uptime.plus/terms" class="px-5" target="_blank">Правила использования</a>
+							<a href="https://uptime.plus/tariffs" class="px-5" target="_blank">Тарифы</a>
+							<a href="https://uptime.plus/contacts" class="px-5" target="_blank">Контакты</a>
 						</div>
-						<!--end::Links-->
 					</div>
-					<!--end::Footer-->
 				</div>
-				<!--end::Body-->
 			</div>
-			<!--end::Authentication - Sign-in-->
 		</div>
-		<!--end::Root-->
-		<!--begin::Javascript-->
+		
 		<script>var hostUrl = "/assets/";</script>
-		<!--begin::Global Javascript Bundle(mandatory for all pages)-->
 		<script src="/assets/plugins/global/plugins.bundle.js"></script>
 		<script src="/assets/js/scripts.bundle.js"></script>
-		<!--end::Global Javascript Bundle-->
-		<!--begin::Custom Javascript(used for this page only)-->
+		
 		<script src="/assets/js/custom/authentication/sign-in/general.js"></script>
-		<!--end::Custom Javascript-->
-		<!--end::Javascript-->
 	</body>
 	<!--end::Body-->
 </html>
